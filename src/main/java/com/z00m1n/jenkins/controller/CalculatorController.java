5b509ae37feff28ddb5fc5d1c4890c0e753e130b
z00m-1n <jmlee_1230@ewha.ac.kr>
2025-05-22 09:15:16 +0900

    second-day

--- src/main/java/com/z00m1n/jenkins/controller/CalculatorController.java
+++ src/main/java/com/z00m1n/jenkins/controller/CalculatorController.java
@@ -17,7 +17,7 @@ public class CalculatorController {
 
     @GetMapping("/health")
     public String healthCheck() {
-        return "I'm alive";
+        return "jenkins pipeline test";
     }
 
     @GetMapping("/plus")
